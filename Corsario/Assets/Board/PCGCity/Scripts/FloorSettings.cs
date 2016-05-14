c2baa185886ac4cd9bd71d7492d766f4c859e3b1
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2016-05-14 18:35:07 -0400

    New PCG

---
+++ Corsario/Assets/Board/PCGCity/Scripts/FloorSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class FloorSettings{
+    public string nameFloorTile;
+    public GameObject[] floorTile;
+    public GameObject floorLimit;
+    public GameObject floorLimit2;
+    public GameObject background;
+    
+}
